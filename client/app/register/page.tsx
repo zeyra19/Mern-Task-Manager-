a3d1aa0087f1204e6322b6c03d6a2301d55da56a
zeyra19 <zehranurhindioglu@gmail.com>
2025-01-09 01:14:44 +0300

    Add overdue and pending tasks

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import RegisterForm from "../Components/auth/RegisterForm/RegisterForm";
+import { useUserContext } from "@/context/userContext";
+import { useRouter } from "next/navigation";
+
 
 function page() {
+  const { user } = useUserContext();
+  const router = useRouter();
+  // Kullanıcı oturum açtıysa ana sayfaya yönlendir
+  useEffect(() => {
+    if (user && user._id) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  // Kullanıcı giriş yaptıysa boş dön
+  if (user && user._id) {
+    return null;
+  }
+
   return (
     <div className="auth-page w-full h-full flex justify-center items-center">
       <RegisterForm />
