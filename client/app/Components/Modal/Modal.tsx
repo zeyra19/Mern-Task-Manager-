005d639f5b05e336b7d08b4c0c2b67e16e1c2352
zeyra19 <zehranurhindioglu@gmail.com>
2025-01-11 13:24:41 +0300

    Create sidebar and header

---
+++ client/app/Components/Modal/Modal.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useTasks } from "@/context/taskContext";
+import useDetectOutside from "@/hooks/useDetectOutside";
+import React, { useEffect } from "react";
+
+function Modal() {
+  const = useTasks();
+  const ref = React.useRef(null)
+}
