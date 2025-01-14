fa5f66d4fbbf1bc4427e7b00b32928cb601f572a
zeyra19 <zehranurhindioglu@gmail.com>
2025-01-14 21:51:40 +0300

    Add server url

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -8,7 +8,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://taskfyer.onrender.com";
+  const serverUrl = "https://taskfyer-0k4p.onrender.com";
 
   const router = useRouter();
 
